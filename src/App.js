d5fd3c501ac64b09f9f8d7ffecd3a0df7454f8c5
Valarie Jepleting <valariejepleting@gmail.com>
2025-07-21 12:29:43 +0300

    Creates React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Deliveroo Frontend Setup Successful! 🚀
+      </h1>
     </div>
   );
 }
