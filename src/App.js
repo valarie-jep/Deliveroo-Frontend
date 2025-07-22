84f0d4c25c6d5aa10bb019a2d7d8af30d25dee95
EVANS KIPKORIR <EVANOCRUSE@GMAIL.COM>
2025-07-22 16:49:17 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,56 +1,50 @@
+import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
-} from 'react-router-dom';
-import { useSelector } from 'react-redux';
+  Navigate,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+import Dashboard from "./components/Dashboard";
+import authReducer from "./redux/authSlice";
 
-const Login = () => <div>Login Page</div>;
-const Register = () => <div>Register Page</div>;
-const Dashboard = () => <div>Dashboard Page</div>;
-const Parcel = () => <div>Parcel Details Page</div>;
-const Admin = () => <div>Admin Page</div>;
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
 
-function PrivateRoute({ children }) {
-  const token = useSelector((state) => state.auth.token);
+const AuthWrapper = ({ children }) => {
+  const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/login" replace />;
-}
+};
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/parcel/:id"
-          element={
-            <PrivateRoute>
-              <Parcel />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <PrivateRoute>
-              <Admin />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/dashboard" replace />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <AuthWrapper>
+                <Dashboard />
+              </AuthWrapper>
+            }
+          />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
+;
 
 export default App;
