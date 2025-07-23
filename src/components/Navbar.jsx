c53e1fa8624122d9fa5b8fa1f24a75d4550cffed
Allan <allanbarongo5@gmail.com>
2025-07-23 23:53:37 +0300

    Implemented Navbar and footer structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+        <div>
+            <Link to="/">Deliveroo</Link>
+        </div>
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/parcels">My Parcels</Link>
+            <Link to="/login">LogIn</Link>
+            <Link to="/register">Get Started</Link>
+            <Link to="/contactUs">Contact Us</Link>
+            
+        </div>
+    </nav>
   )
 }
 
