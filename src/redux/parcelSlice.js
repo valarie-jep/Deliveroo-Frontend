5f8ed20fba83c7128a9b7ed9370f31fa9b35d849
Allan <allanbarongo5@gmail.com>
2025-07-24 19:04:04 +0300

    Changed base React url to the deployed link

--- src/redux/parcelSlice.js
+++ src/redux/parcelSlice.js
@@ -2,8 +2,8 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 
-//const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000';
-const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'https://deliveroo-server.onrender.com/';
+
 
 // Helper to get auth token
 const getAuthHeaders = (thunkAPI) => {
