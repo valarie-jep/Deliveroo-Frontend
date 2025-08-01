153d1673cc23db2709a8aefe0d04661fb3a2be9a
Allan Barongo <122985155+A-Barongo@users.noreply.github.com>
2025-08-01 12:43:36 +0300

    Update App.jsx
    Reverted changes

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,7 @@ import EmailSettings from "./pages/EmailSettings.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { LoadScript } from "@react-google-maps/api";
 
-
-const libraries = ["places"];
 // Route guards
 const AuthWrapper = ({ children }) => {
   const token = localStorage.getItem("token");
@@ -66,10 +63,7 @@ const AdminRedirectRoute = ({ children }) => {
 const App = () => {
   return (
     <Provider store={store}>
-      <LoadScript
-        googleMapsApiKey={process.env.REACT_APP_Maps_API_KEY}
-        libraries={libraries}
-      >
+      
       <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
@@ -159,7 +153,7 @@ const App = () => {
         {/* Global toast container — place once, near the root */}
         <ToastContainer />
       </Router>
-      </LoadScript>
+      
     </Provider>
   );
 };
